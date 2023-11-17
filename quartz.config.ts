caacd901d208a3ccc21b4e76075b0e870c870d65
ecoplunge <64333414+ecoplunge@users.noreply.github.com>
2023-11-17 07:03:02 -0600

    change site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Porpoise Unknown",
+    pageTitle: "Porpoise Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
